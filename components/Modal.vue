39aa5dc7cc4f1005f365b98406cf91202255ae74
Jorge Castilla <jorgecastillagallardo@gmail.com>
2022-10-27 14:44:43 -0300

    add send mail working

--- components/Modal.vue
+++ components/Modal.vue
@@ -2,13 +2,15 @@
 
     <div>
         <b-modal id="modal-contact" hide-footer hide-header>
-            
+
+
             <h5 class="my-2 mx-2">¡Hablemos!</h5>
             <b-row class="my-2 mx-2" v-for="input in inputs" :key="input.input">
 
                 <label v-if="input.input != 'Mensaje'" :for="input.input">{{ input.input }}:</label>
 
-                <b-form-input v-if="input.input != 'Mensaje'" :id="`type-${input.input}`" :type="input.type" v-model="input.value">
+                <b-form-input v-if="input.input != 'Mensaje'" :id="`type-${input.input}`" :type="input.type"
+                    v-model="input.value">
                 </b-form-input>
 
                 <b-form-textarea v-else id="textarea" v-model="input.value" placeholder="Escribe tu mensaje..." rows="3"
@@ -16,14 +18,15 @@
 
 
             </b-row>
-            <button @click="sendForm" class="my-2 mx-2">Enviar</button>
+            <button @click="sendEmail" class="my-2 mx-2">Enviar</button>
 
         </b-modal>
     </div>
 </template>
 
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
@@ -36,8 +39,36 @@ export default {
         }
     },
     methods: {
-        sendForm() {
-            this.$bvModal.hide('modal-contact');
+       async sendEmail() {
+            var data = {
+                service_id: 'service_ofkczni',
+                template_id: 'template_2u4jly3',
+                user_id: 'oLoYhzJdcjeEdg91w',
+                template_params: {
+                    'name': this.inputs[0].value,
+                    'email': this.inputs[1].value,
+                    'subject': this.inputs[2].value,
+                    'message': this.inputs[3].value
+                }
+            };
+            var config = {
+                method: 'post',
+                url: 'https://api.emailjs.com/api/v1.0/email/send',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Request-Headers': '*',
+                },
+                data: data
+            };
+            try {
+                const response = await axios(config);
+                console.log(response);
+                this.$bvModal.hide('modal-contact');
+            } catch (error) {
+                console.log(error);
+            }
+
+
         }
     }
 
