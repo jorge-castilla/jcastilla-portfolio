95928c9c542eef6fa10b325d500d44f2122287e9
Jorge Castilla <jorgecastillagallardo@gmail.com>
2022-11-12 05:23:17 -0300

    add scroll to top

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,25 +1,34 @@
 <template>
-  <nav id="navbar">
-    <div>
-        <p class="name-tag">
-            JCastilla
-        </p>
+    <nav id="navbar">
+        <div>
+            <p class="name-tag" @click="scrollToTop">
+                JCastilla
+            </p>
 
-    </div>
+        </div>
 
-  </nav>
+    </nav>
 </template>
 
 <script>
 export default {
-    props:{
+    props: {
     },
-    data(){
-        return{
+    data() {
+        return {
 
         }
     },
-    watch:{
+    methods: {
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            })
+        }
+    },
+    watch: {
 
     }
 
@@ -27,13 +36,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#navbar{
+#navbar {
     display: flex;
     justify-content: space-between;
     position: fixed;
     width: 100%;
     z-index: 10;
-    .name-tag{
+
+    .name-tag {
         background: var(--pink);
         color: var(--white);
         font-size: 6vh;
@@ -41,33 +51,45 @@ export default {
         padding: 0px 0.2em;
         transform: scale(1, 1.1);
         cursor: pointer;
-        &:hover{
+
+        &:hover {
             background: var(--hover-pink);
         }
     }
-        ul {
-            color: var(--white);
-            font-family: 'futura-md';
-            font-size: 24px;
-            list-style-type: none;
-            margin-top: 1rem;
-            padding: 0px 3rem;
-            position: relative;
-            .active-bg{
-                background: var(--white);
-                padding-left: 100px;
-                position: absolute;
-                width: 100%;
-                transition: 0.2s;
-                z-index: -1;
+
+    ul {
+        color: var(--white);
+        font-family: 'futura-md';
+        font-size: 24px;
+        list-style-type: none;
+        margin-top: 1rem;
+        padding: 0px 3rem;
+        position: relative;
+
+        .active-bg {
+            background: var(--white);
+            padding-left: 100px;
+            position: absolute;
+            width: 100%;
+            transition: 0.2s;
+            z-index: -1;
+        }
+
+        li {
+            cursor: pointer;
+            font-weight: 500;
+            margin: 0px 0px 1rem 1rem;
+
+            &.active {
+                color: var(--pink);
             }
-            li{
-                cursor: pointer;
-                font-weight: 500;
-                margin: 0px 0px 1rem 1rem;
-                &.active{ color: var(--pink);}
-                svg{ width: 14px;}
-                transition: 0.2s;
+
+            svg {
+                width: 14px;
             }
-        }}
+
+            transition: 0.2s;
+        }
+    }
+}
 </style>
